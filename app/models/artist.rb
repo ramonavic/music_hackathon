f2d93d55c984a8231e26a71e3fe267498462f05f
Dennis <info@dennisdijkstra.nl>
2016-06-15 14:44:17 +0200

    create links, footer,  artist songs relation

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,3 @@
 class Artist < ActiveRecord::Base
-   belongs_to :song
+   has_many :songs
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-   root 'songs#index'
-
+   root 'artists#index'
 
   resources :songs
   resources :artists
