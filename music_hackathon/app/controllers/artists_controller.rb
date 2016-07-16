438d02bd2e2989527dbf7488a37baf9b162390c6
ramonavic <ramon.hofmeister@gmail.com>
2016-07-16 15:26:06 +0200

    merge

--- music_hackathon/app/controllers/artists_controller.rb
+++ music_hackathon/app/controllers/artists_controller.rb
@@ -11,29 +11,34 @@ class ArtistsController < ApplicationController
 
   def show
 
-    @artists = Artist.all
     @artist = Artist.find(params[:id])
 
-    render = @artist.name.downcase
+    render = @artist.name.downcase.capitalize
+
     wikiurl = "https://en.wikipedia.org/wiki/#{render.to_s.tr(' ', '_')}"
     response1 = Net::HTTP.get_response(URI(wikiurl))
 
+    puts response1.to_s
+
     case response1
     when Net::HTTPSuccess
-      @data = Nokogiri::HTML(open(@wikiurl))
-      @wikiscrape = @data.css('#mw-content-text')
+      data1 = Nokogiri::HTML(open(wikiurl))
+      @wikiscrape = data1.css('#mw-content-text')
+      puts @wikiscrape
     when Net::HTTPRedirection
       @wikiscrape = ""
+      puts "jezus"
     end
 
 
-    mtvurl = "http://www.mtv.com/artists/#{render.parameterize.to_s}/"
+    mtvurl = "http://www.mtv.com/artists/#{render.downcase.parameterize.to_s}/"
     response2 = Net::HTTP.get_response(URI(mtvurl))
 
     case response2
     when Net::HTTPSuccess
-      @data = Nokogiri::HTML(open(mtvurl))
-      @mtvnews = @data.css("#profile_latest_news")
+      data2 = Nokogiri::HTML(open(mtvurl))
+      @mtvscrape = data2.css(".tourdate-item")
+      @mtvnews = data2.css("#profile_latest_news")
     when Net::HTTPRedirection
       @mtvscrape = ""
       @mtvnews = ""
@@ -45,8 +50,8 @@ class ArtistsController < ApplicationController
 
     case response3
     when Net::HTTPSuccess
-      @news = Nokogiri::HTML(open(mtvnews))
-      @mtvnewslink = @news.at_css(".content-body")
+      news = Nokogiri::HTML(open(mtvnews))
+      @mtvnewslink = news.at_css(".content-body")
     when Net::HTTPRedirection
       @mtvnewslink = ""
     end
@@ -57,8 +62,8 @@ class ArtistsController < ApplicationController
 
     case response4
     when Net::HTTPSuccess
-      @data = Nokogiri::HTML(open(rollingstone))
-      @images = @data.css(".main")
+      data4 = Nokogiri::HTML(open(rollingstone))
+      @images = data4.css(".main")
     when Net::HTTPRedirection
       @images = ""
     end
