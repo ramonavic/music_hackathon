d2d651e665641db519264b29fe3b1e9d4da9b5b1
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-07-16 09:43:12 +0200

    Fix error 404

--- music_hackathon/db/seeds.rb
+++ music_hackathon/db/seeds.rb
@@ -9,3 +9,5 @@
 frank = User.create( email: 'frank@ex.com', password: 'abcd1234' )
 alice = User.create( email: 'alice@ex.com', password: 'abcd1234' )
 anton = User.create( email: 'anton@ex.com', password: 'abcd1234' )
+
+Artist.create( name: 'Rihanna' )
